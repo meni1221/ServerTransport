220a6f1b060b526500c3a97835e5adfb77561bab
meni_levi <1224tap@gmail.com>
2024-12-01 13:01:00 +0200

    Add route from route

--- router/router.ts
+++ router/router.ts
@@ -1,6 +1,7 @@
 import express, { IRouter, NextFunction, Request, Response }  from 'express'
 import usersContoller from "../src/controllers/usersContoller"
 import busesController from "../src/controllers/busesController"
+import routesController from "../src/controllers/routesController"
 import authController from "../src/controllers/authController"
 import { verifyAdmin, verifyUser } from '../helpers/jwt';
 import { handleError } from '../utils/ErrorHandle';
@@ -9,6 +10,7 @@ const router:IRouter = express.Router()
 
 router.use("/users",verifyUser as NextFunction,usersContoller );
 router.use("/buses",verifyUser as NextFunction,busesController );
+router.use("/routes",verifyUser as NextFunction,routesController );
 router.use("/admin-role",verifyAdmin as NextFunction,usersContoller );
 router.use("/auth",authController );
 
