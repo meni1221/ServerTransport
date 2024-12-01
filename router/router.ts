9bd5801413f41c1e6c3a9e68015310fed1e60ade
meni_levi <1224tap@gmail.com>
2024-12-01 12:08:32 +0200

    update route to users

--- router/router.ts
+++ router/router.ts
@@ -1,13 +1,13 @@
 import express, { IRouter, NextFunction, Request, Response }  from 'express'
-import dataContoller from "../src/controllers/dataContoller"
+import usersContoller from "../src/controllers/usersContoller"
 import authController from "../src/controllers/authController"
 import { verifyAdmin, verifyUser } from '../helpers/jwt';
 import { handleError } from '../utils/ErrorHandle';
 
 const router:IRouter = express.Router()
 
-router.use("/data",verifyUser as NextFunction,dataContoller );
-router.use("/admin-role",verifyAdmin as NextFunction,dataContoller );
+router.use("/users",verifyUser as NextFunction,usersContoller );
+router.use("/admin-role",verifyAdmin as NextFunction,usersContoller );
 router.use("/auth",authController );
 
 router.use((req:Request,res:Response)=>{
