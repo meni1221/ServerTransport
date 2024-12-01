364f4e7f8734d6299b27d85dc47849f8e00823af
meni_levi <1224tap@gmail.com>
2024-12-01 12:43:11 +0200

    Add routr from buses

--- router/router.ts
+++ router/router.ts
@@ -1,5 +1,6 @@
 import express, { IRouter, NextFunction, Request, Response }  from 'express'
 import usersContoller from "../src/controllers/usersContoller"
+import busesController from "../src/controllers/busesController"
 import authController from "../src/controllers/authController"
 import { verifyAdmin, verifyUser } from '../helpers/jwt';
 import { handleError } from '../utils/ErrorHandle';
@@ -7,6 +8,7 @@ import { handleError } from '../utils/ErrorHandle';
 const router:IRouter = express.Router()
 
 router.use("/users",verifyUser as NextFunction,usersContoller );
+router.use("/buses",verifyUser as NextFunction,busesController );
 router.use("/admin-role",verifyAdmin as NextFunction,usersContoller );
 router.use("/auth",authController );
 
