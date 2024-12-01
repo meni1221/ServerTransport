79404904ae001259aa3dc29497410726b5c623ed
meni_levi <1224tap@gmail.com>
2024-12-01 12:42:38 +0200

    update

--- src/interface/IBus.ts
+++ src/interface/IBus.ts
@@ -1,9 +1,10 @@
 export interface IBus extends Document {
   licensePlate: string;
+  model: string;
   capacity: number;
   status: string;
   driverID: string | object;
-  routelID: string | object;
+  routeID: string | object;
   createdAt: Date;
   updatedAd: Date;
 }
