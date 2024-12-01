716508edf7de1789ca88f2354d951b320dc4e793
meni_levi <1224tap@gmail.com>
2024-12-01 10:04:17 +0200

    Created a busSchema

---
+++ src/models/bus.ts
@@ -0,0 +1,43 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IBus } from "../interface/IBus";
+
+const busSchema: Schema = new Schema(
+  {
+    licensePlate: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    capacity: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    driverID: {
+      type: String || Object,
+      required: true,
+    },
+    routelID: {
+      type: String || Object,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    updatedAd: {
+      type: Date,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IBus>("User", userSchema);
